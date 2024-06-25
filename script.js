833b55bd2d3e0440f3e310712dd9d7eac8fe85fe
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-06-25 19:24:15 +0300

    complete main task

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("111");
+const cross = document.querySelector(".clear");
+const resWrapper = document.querySelector(".results-wrapper");
+const input = document.querySelector(".form-control");
+const searchForm = document.querySelector("#search_form");
+
+function showCross() {
+    cross.style.display = "block";
+}
+
+function hiddenCross() {
+    cross.style.display = "none";
+}
+
+function clearFormAndWrapper() {
+    input.value = "";
+    resWrapper.innerHTML = "";
+    hiddenCross();
+}
+
+if (input.value.length > 0) {
+    showCross();
+}
+
+
+cross.addEventListener("click", () => {
+    clearFormAndWrapper();
+})
+
+input.addEventListener("input", () => {
+        const inputLength = input.value.length;
+        if (inputLength <= 0) {
+            hiddenCross();
+        } else {
+            showCross();
+        }
+})
+
+window.addEventListener("click", (event) => {
+    if (!searchForm.contains(event.target)) {
+        clearFormAndWrapper();
+    }
+} )
